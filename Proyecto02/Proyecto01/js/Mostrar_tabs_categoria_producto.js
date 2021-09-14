b583faf9cd6e988652394431b8a56034f8bf23e2
chris-merge <chrishale1997j2@gmail.com>
2021-09-13 21:55:24 -0600

    se agrego catalogo y funcion de js y css de tabs

---
+++ Proyecto02/Proyecto01/js/Mostrar_tabs_categoria_producto.js
@@ -0,0 +1,42 @@
+/*funcion para mostrar los tabs de categoria espesifica*/
+function MostrarTabs(id){
+switch (id) {
+    case "t1":
+        document.getElementById("categoria").style.display="block";
+        document.getElementById("test1").style.display="block";
+        document.getElementById("test2").style.display="none";
+        document.getElementById("test3").style.display="none";
+        document.getElementById("Catalogo").style.display="none";
+        document.getElementById("test4").style.display="none";
+        break;
+    case "t2":
+        document.getElementById("categoria").style.display="block";
+        document.getElementById("test1").style.display="none";
+        document.getElementById("test2").style.display="block";
+        document.getElementById("test3").style.display="none";
+        document.getElementById("Catalogo").style.display="none";
+        document.getElementById("test4").style.display="none";
+        break;
+    case "t3":
+        document.getElementById("categoria").style.display="block";
+        document.getElementById("test1").style.display="none";
+        document.getElementById("test2").style.display="none";
+        document.getElementById("test3").style.display="block";
+        document.getElementById("Catalogo").style.display="none";
+        document.getElementById("test4").style.display="none";
+        break;
+    default:
+        document.getElementById("categoria").style.display="none";
+        break;
+}
+};
+/*funcion para mostrar los tabs de categoria espesifica*/
+/*ocultar todos los tabs de categoria espesifica y mustre todo lo general Catalogo*/
+function OcultarTabs(){
+    document.getElementById("categoria").style.display="none";
+    document.getElementById("test1").style.display="none";
+    document.getElementById("test2").style.display="none";
+    document.getElementById("test3").style.display="none";
+    document.getElementById("Catalogo").style.display="block";
+    document.getElementById("test4").style.display="block";
+};
